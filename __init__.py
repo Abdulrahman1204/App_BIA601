3ee4de3f6c3e0fff309d542108e881446acc3f69
salehaljazery-stack <salehaljazaery@gmail.com>
2025-11-05 12:42:29 +0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-"""
-حزمة الخوارزميات
-Algorithms Package
-"""
-from .genetic_feature_selection import GeneticFeatureSelection
-from .traditional_methods import TraditionalFeatureSelection
-
-__all__ = ['GeneticFeatureSelection', 'TraditionalFeatureSelection']
-
